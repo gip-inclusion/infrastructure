5a4235ac3cd99570ee6533c927c4700c4107acb8
David Martin Cohen <davidmartincohen@gmail.com>
2025-08-28 17:22:58 +0200

    feat: add 4 new dora subdomains
    * staging/prod for api and website

--- infrastructure/iac-gip-inclusion/dns/dora/terraform/main.tf
+++ infrastructure/iac-gip-inclusion/dns/dora/terraform/main.tf
@@ -27,5 +27,29 @@ module "dns-dora" {
       type = "NS"
       ttl  = 1800
     },
+    "prod-website" = {
+      name = "dora"
+      data = "dora-front-prod.osc-secnum-fr1.scalingo.io."
+      type = "ALIAS"
+      ttl = 300
+    },
+    "staging-website" = {
+      name = "staging.dora"
+      data = "dora-front-staging.osc-secnum-fr1.scalingo.io."
+      type = "CNAME"
+      ttl = 300
+    },
+    "prod-api" = {
+      name = "api.dora"
+      data = "dora-back-prod.osc-secnum-fr1.scalingo.io."
+      type = "CNAME"
+      ttl = 300
+    },
+    "staging-api" = {
+      name = "api.staging.dora"
+      data = "dora-back-staging.osc-secnum-fr1.scalingo.io."
+      type = "CNAME"
+      ttl = 300
+    },
   }
 }
