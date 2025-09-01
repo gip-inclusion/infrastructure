6bbaeee0927c0a980b506760f987f56633527dc8
Léo S. <leo@naeka.fr>
2025-09-01 12:02:37 +0200

    ops(dns): remove DORA zone delegation
    DORA zone does not exist anymore

--- infrastructure/iac-gip-inclusion/dns/dora/terraform/main.tf
+++ infrastructure/iac-gip-inclusion/dns/dora/terraform/main.tf
@@ -15,47 +15,35 @@ module "dns-dora" {
   scw_zone   = var.scw_zone
 
   records = {
-    "ns0" = {
-      name = "dora"
-      data = "ns0.dom.scw.cloud."
-      type = "NS"
-      ttl  = 1800
-    },
-    "ns1" = {
-      name = "dora"
-      data = "ns1.dom.scw.cloud."
-      type = "NS"
-      ttl  = 1800
-    },
     "prod-website" = {
       name = "dora"
       data = "dora-front-prod.osc-secnum-fr1.scalingo.io."
       type = "ALIAS"
-      ttl = 300
+      ttl  = 300
     },
     "staging-website" = {
       name = "staging.dora"
       data = "dora-front-staging.osc-secnum-fr1.scalingo.io."
       type = "CNAME"
-      ttl = 300
+      ttl  = 300
     },
     "prod-api" = {
       name = "api.dora"
       data = "dora-back-prod.osc-secnum-fr1.scalingo.io."
       type = "CNAME"
-      ttl = 300
+      ttl  = 300
     },
     "staging-api" = {
       name = "api.staging.dora"
       data = "dora-back-staging.osc-secnum-fr1.scalingo.io."
       type = "CNAME"
-      ttl = 300
+      ttl  = 300
     },
     "metabase" = {
       name = "metabase.dora"
       data = "dora-metabase-v2.osc-secnum-fr1.scalingo.io."
       type = "CNAME"
-      ttl = 300
+      ttl  = 300
     },
   }
 }
