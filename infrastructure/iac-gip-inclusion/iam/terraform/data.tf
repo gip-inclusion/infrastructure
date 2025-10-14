7d3985b9f92319ec6da9ccc1a27332538a550673
François Freitag <mail@franek.fr>
2025-10-14 14:41:02 +0200

    Allow terraform-ci to handle site-institutionnel-2025 buckets

--- infrastructure/iac-gip-inclusion/iam/terraform/data.tf
+++ infrastructure/iac-gip-inclusion/iam/terraform/data.tf
@@ -9,3 +9,7 @@ data "scaleway_account_project" "terraform" {
 data "scaleway_account_project" "emplois_cnav" {
   name = "emplois-cnav"
 }
+
+data "scaleway_account_project" "site_institutionnel_2025" {
+  name = "site-institutionnel-2025"
+}
