e2ee8261c08b715435bf9cee3cfbf88414fabee3
Léo S. <leo@naeka.fr>
2025-10-22 15:30:18 +0200

    ops(iam): create application for Github backups

--- infrastructure/iac-gip-inclusion/iam/terraform/data.tf
+++ infrastructure/iac-gip-inclusion/iam/terraform/data.tf
@@ -13,3 +13,8 @@ data "scaleway_account_project" "emplois_cnav" {
 data "scaleway_account_project" "site_institutionnel_2025" {
   name = "site-institutionnel-2025"
 }
+
+data "scaleway_secret" "github_backups_api_key" {
+  name       = "github-backups-api-key"
+  project_id = data.scaleway_account_project.default.id
+}
