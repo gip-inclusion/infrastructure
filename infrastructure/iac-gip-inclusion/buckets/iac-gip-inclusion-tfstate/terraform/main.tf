06eda36001b7fc8f8a90dfbcb0b9b9d053630237
Léo S. <leo@naeka.fr>
2025-11-11 02:03:34 +0100

    ops(iam): allow emplois-cnav developers to read and write emplois-cnav project states

--- infrastructure/iac-gip-inclusion/buckets/iac-gip-inclusion-tfstate/terraform/main.tf
+++ infrastructure/iac-gip-inclusion/buckets/iac-gip-inclusion-tfstate/terraform/main.tf
@@ -60,6 +60,45 @@ resource "scaleway_object_bucket_policy" "state_bucket_policy" {
             "${scaleway_object_bucket.gip_inclusion_terraform_state.name}/*"
           ]
         },
+        {
+          Sid    = "Allow emplois-cnav developers to access the state bucket",
+          Effect = "Allow",
+          Principal = {
+            SCW = [
+              "user_id:c10ec0b8-a110-4d2e-85a2-f896ce77dfd1",
+            ]
+          },
+          Action = [
+            "s3:GetBucketAcl",
+            "s3:GetBucketCORS",
+            "s3:GetBucketLocation",
+            "s3:GetBucketObjectLockConfiguration",
+            "s3:GetBucketTagging",
+            "s3:GetBucketVersioning",
+            "s3:GetBucketWebsite",
+            "s3:GetLifecycleConfiguration",
+            "s3:ListBucket",
+          ]
+          Resource = [
+            scaleway_object_bucket.gip_inclusion_terraform_state.name,
+          ]
+        },
+        {
+          Sid    = "Allow emplois-cnav developers to read and write states relative to the emplois-cnav project",
+          Effect = "Allow",
+          Principal = {
+            SCW = [
+              "user_id:c10ec0b8-a110-4d2e-85a2-f896ce77dfd1",
+            ]
+          },
+          Action = [
+            "s3:GetObject",
+            "s3:PutObject",
+          ]
+          Resource = [
+            "${scaleway_object_bucket.gip_inclusion_terraform_state.name}/emplois-cnav/*"
+          ]
+        },
       ]
     }
   )
