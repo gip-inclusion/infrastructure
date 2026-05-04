e0cc206148369a26fcb30c056f2b79ef78d44fa0
Léo S. <leo@naeka.fr>
2026-05-04 18:15:22 +0200

    ops(dns): add traiteurs-engages staging email DNS
    DMARC policy inherit from inclusion.gouv.fr

--- infrastructure/iac-gip-inclusion/dns/traiteurs-engages/terraform/main.tf
+++ infrastructure/iac-gip-inclusion/dns/traiteurs-engages/terraform/main.tf
@@ -26,5 +26,22 @@ module "dns-traiteurs-engages" {
       data = "proxy.applicatif.net."
       type = "CNAME"
     },
+
+    # Emails
+    "staging-brevo-code" : {
+      name = "staging.traiteurs-engages"
+      data = "brevo-code:96720ae72c4b9e35f0b138dac0f441c4"
+      type = "TXT"
+    },
+    "staging-spf" : {
+      name = "staging.traiteurs-engages"
+      data = "v=spf1 include:spf.brevo.com -all"
+      type = "TXT"
+    },
+    "staging-dkim" : {
+      name = "mail._domainkey.staging.traiteurs-engages"
+      data = "k=rsa;p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDeMVIzrCa3T14JsNY0IRv5/2V1/v2itlviLQBwXsa7shBD6TrBkswsFUToPyMRWC9tbR/5ey0nRBH0ZVxp+lsmTxid2Y2z+FApQ6ra2VsXfbJP3HE6wAO0YTVEJt1TmeczhEd2Jiz/fcabIISgXEdSpTYJhb0ct0VJRxcg4c8c7wIDAQAB"
+      type = "TXT"
+    },
   }
 }
