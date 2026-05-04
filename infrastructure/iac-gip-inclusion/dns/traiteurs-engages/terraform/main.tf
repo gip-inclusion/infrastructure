352635a1d33f796666218b0908e84155da3cfbe1
Léo S. <leo@naeka.fr>
2026-05-04 17:58:09 +0200

    chore(dns): drop import blocks from traiteurs-engages module

--- infrastructure/iac-gip-inclusion/dns/traiteurs-engages/terraform/main.tf
+++ infrastructure/iac-gip-inclusion/dns/traiteurs-engages/terraform/main.tf
@@ -28,13 +28,3 @@ module "dns-traiteurs-engages" {
     },
   }
 }
-
-import {
-  to = module.dns-traiteurs-engages.scaleway_domain_record.records["staging"]
-  id = "inclusion.gouv.fr/e0d64f79-928e-4c44-9758-5211f9bb8cd6"
-}
-
-import {
-  to = module.dns-traiteurs-engages.scaleway_domain_record.records["prod"]
-  id = "inclusion.gouv.fr/ea9b0c4a-6f6a-4658-9326-5d3a3fb6d8f9"
-}
