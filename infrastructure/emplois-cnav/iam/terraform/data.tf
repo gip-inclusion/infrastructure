764cea8601db1c97805cd19c7877846796fa5821
Léo S. <leo@naeka.fr>
2026-06-12 23:15:26 +0200

    fixup! ops(emplois-cnav): add iam module
    Add kubernetes users group + members

--- infrastructure/emplois-cnav/iam/terraform/data.tf
+++ infrastructure/emplois-cnav/iam/terraform/data.tf
@@ -2,3 +2,12 @@ data "scaleway_account_project" "emplois_cnav" {
   name     = "emplois-cnav"
   provider = scaleway.tmp
 }
+
+data "scaleway_iam_user" "kubernetes_users" {
+  count = length(local.kubernetes_users_emails)
+  email = local.kubernetes_users_emails[count.index]
+}
+
+data "sops_file" "secrets" {
+  source_file = "secrets.enc.yaml"
+}
